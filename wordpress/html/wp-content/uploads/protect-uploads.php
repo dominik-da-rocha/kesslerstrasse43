2047bb595ad3a7b6dfb4f880aaf1bf8b66634b6a
Dominik da Rocha <dominik.da.rocha@gmail.com>
2022-12-06 22:10:35 +0100

    - adjusted chacje time to 1h

--- wordpress/html/wp-content/uploads/protect-uploads.php
+++ wordpress/html/wp-content/uploads/protect-uploads.php
@@ -40,7 +40,7 @@ $last_modified = gmdate('D, d M Y H:i:s', filemtime($file));
 $etag = '"' . md5($last_modified) . '"';
 header("Last-Modified: $last_modified GMT");
 header('ETag: ' . $etag);
-header('Expires: ' . gmdate('D, d M Y H:i:s', time() + 100000000) . ' GMT');
+header('Expires: ' . gmdate('D, d M Y H:i:s', time() + 3600) . ' GMT');
 
 // Support for Conditional GET
 $client_etag = isset($_SERVER['HTTP_IF_NONE_MATCH']) ? stripslashes($_SERVER['HTTP_IF_NONE_MATCH']) : false;
