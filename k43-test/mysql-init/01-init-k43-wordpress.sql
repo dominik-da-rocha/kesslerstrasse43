9d0ff51c0316bd33c97bf73001c038653409d610
Rocha <dominik.da.rocha@gmail.com>
2022-12-10 18:08:38 +0100

    removed test environment

--- k43-test/mysql-init/01-init-k43-wordpress.sql
+++
@@ -1,5 +0,0 @@
--- Initialize Keyclock db
-CREATE USER 'k43_wordpress_user' IDENTIFIED BY '1234';
-CREATE DATABASE IF NOT EXISTS k43_wordpress_db;
-GRANT ALL PRIVILEGES ON k43_wordpress_db.* TO 'k43_wordpress_user';
-
